19cfef30b9d43284a5be11227b065e08ad1a2f53
christianT30 <3kuma30@gmail.com>
2024-09-15 22:59:45 -0300

    Agregar proyecto ObraSocialPersistencia

---
+++ src/main/java/com/mycompany/obrasocialpersistencia/ObraSocialPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.obrasocialpersistencia;
+
+/**
+ *
+ * @author Christian
+ */
+public class ObraSocialPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

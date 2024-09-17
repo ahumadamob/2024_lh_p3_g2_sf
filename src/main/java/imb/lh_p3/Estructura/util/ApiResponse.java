97ade4e7f6a7c7929e2c5cd380f6969fd16220b1
Mario Ahumada <ahumadamob@gmail.com>
2024-09-17 20:45:26 -0300

    ApiResponse

--- src/main/java/imb/lh_p3/Estructura/util/ApiResponse.java
+++ src/main/java/imb/lh_p3/Estructura/util/ApiResponse.java
@@ -1,23 +1,23 @@
-package imb.lh_p3.Estructura.util;
+package imb.lh_p3.Estructura.Util;
 
 public class ApiResponse<T> {
-
-		private String error;
-		private T data;
-		
-		
-		public String getError() {
-			return error;
-		}
-		public void setError(String error) {
-			this.error = error;
-		}
-		public T getData() {
-			return data;
-		}
-		public void setData(T data) {
-			this.data = data;
-		}
-		
+	
+	private String error;
+	private T data;
+	
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	
+	
 
 }
